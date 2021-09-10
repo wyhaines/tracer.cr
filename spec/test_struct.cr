9abfc8769a7fb1e33454182f5227c68f3dc963eb
Kirk Haines <wyhaines@gmail.com>
2021-09-10 12:09:26 -0600

    Add missing spec files.

---
+++ spec/test_struct.cr
@@ -0,0 +1,26 @@
+struct TestStruct
+  property value
+
+  def initialize(@value : String)
+  end
+
+  def framed_value
+    "|#{value}|"
+  end
+
+  def self.random
+    new(Random.new.urlsafe_base64)
+  end
+end
+
+struct TestStruct
+  trace(
+    "framed_value",
+    ->() {ExternalTraceManager.log(__trace_method_receiver__, __trace_method_identifier__, __trace_method_call_counter__)}
+  )
+end
+
+trace(
+  "TestStruct.random",
+  ->() {ExternalTraceManager.log(__trace_method_receiver__, __trace_method_identifier__, __trace_method_call_counter__)}
+)
